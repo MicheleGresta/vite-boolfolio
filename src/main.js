90eb1223ef81e61e3cd331d9f2d7eef898f0f290
MicheleGresta <malachia_1997@hotmail.it>
2023-10-16 15:53:03 +0200

    init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import { createApp } from 'vue'
+import './style.scss'
+import App from './App.vue'
+import 'bootstrap'
+
+createApp(App).mount('#app')
